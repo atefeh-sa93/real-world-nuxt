9f0c08f026a8f1de6e424bb83801fb624c2f2e73
atefeh <ati.atashi1993@yahoo.com>
2020-08-22 22:01:16 +0430

    register eventCard component in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard v-for="(event, index) in events" :key="index" :event="event" :data-index="index" />
   </div>
 </template>
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
+  components: {
+    EventCard,
+  },
   head() {
     return {
       title: 'Event Listening',
     }
+  },
+
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3030/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({ statusCode: 503, message: 'Unable to show!' })
+      })
   },
 }
 </script>
